54508772dce1bedaeaedae3505e68d8b7cfdcc01
VadimUa <vadia62beck@gmail.com>
2021-05-21 02:38:10 +0300

    productionReady

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,26 @@
 "use strict"
+function scroll(elem){
+    window.scrollTo({
+        top: elem.offsetTop,
+        behavior: "smooth"
+    });
+}
+let secondPart = document.querySelector('.white_line');
+let mapnure = document.querySelector('.map__nure');
 let overlay = document.querySelector(".map__nure_overlay");
 overlay.addEventListener("click", () =>{
     overlay.classList.add("active");
+})
+
+let downArrow = document.querySelector('.buttondown');
+downArrow.addEventListener('click',() => {
+    scroll(secondPart);
+})
+
+let know = document.querySelector('.jsbutton');
+know.addEventListener('click',() => {
+    window.scrollTo({
+        top:mapnure.offsetTop,
+        behavior:'smooth'
+    });
 })
